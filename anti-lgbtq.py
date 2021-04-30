e50fdba546a9e980ea100b5c35a3b38a3352ac41
bruhpatrick <77025787+bruhpatrick@users.noreply.github.com>
2021-04-30 15:24:53 +0300

    Add files via upload
    first ever code, hope you like

---
+++ anti-lgbtq.py
@@ -0,0 +1,9 @@
+hate_or_not = input("do you hate the LGBTQ+ community? ").lower()
+if hate_or_not == "yes":
+    print("You are amazing")
+if hate_or_not == "no":
+    ans = input("are you a a part of the lgbtq+ community? yes/no ")
+    if ans == "no":
+        print ("Good for you")
+    if ans == "yes":
+        print ("KYS RETARD")
